2584ddbf0c229c76528599ce12c3d33471310ede
BenSLGeneration <benjamin.salome@genstudents.org>
2025-01-30 09:39:22 -0300

    Cambios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Administracion from './Pages/Administracion/Administracion';
+
+const App = () => {
+  return (
+    <div>
+      {/* Aqui iria navbar
+      Aqui iria sidebar */}
+      <Administracion />
+    </div>
+  );
+};
+
+export default App;
