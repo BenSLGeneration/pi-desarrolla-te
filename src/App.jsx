ba4a0e64ef0718c3c75264ba0ee2c3b98adb21a5
BenSLGeneration <benjamin.salome@genstudents.org>
2025-01-30 09:44:55 -0300

    Enrutamiento de las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import Administracion from './Pages/Administracion/Administracion';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Administracion from './views/Administracion/Administracion';
+import Dashboard from './views/Dashboard/Dashboard';
+import Reservaciones from './views/Reservaciones/Reservaciones';
+import Login from './views/Login/Login';
 
 const App = () => {
   return (
-    <div>
-      {/* Aqui iria navbar
-      Aqui iria sidebar */}
-      <Administracion />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Redirigir la raíz al login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<><Navbar /><Sidebar /><Dashboard /></>} />
+        <Route path="/reservaciones" element={<><Navbar /><Sidebar /><Reservaciones /></>} />
+        <Route path="/administracion" element={<><Navbar /><Sidebar /><Administracion /></>} />
+
+        {/* Ruta 404 */}
+        <Route path="*" element={<h1>404 - Página no encontrada</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/views/Reservaciones/Reservaciones.jsx
@@ -0,0 +1,9 @@
+const Reservaciones = () => {
+    return (
+      <div>
+        <h1>Se ve el Reservaciones</h1>
+      </div>
+    );
+  };
+  
+export default Reservaciones;
